6e9f12a48ccda3b397fd055900db00c3aa73ca7a
dwynikkoedward <dwynikko.mamangon@gmail.com>
2021-03-07 13:53:03 +0800

    Add Redux Logger Middleware

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware, compose } from 'redux';
+import loggerMiddleware from 'redux-logger';
 import reducer from './reducers';
 import rootSaga from './sagas';
 import createSagaMiddleware from 'redux-saga';
@@ -6,8 +7,12 @@ import createSagaMiddleware from 'redux-saga';
 export default function configureStore() {
     const sagaMiddleware = createSagaMiddleware();
     const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    const middlewares = [
+        loggerMiddleware,
+        sagaMiddleware
+    ]
     const store = createStore(reducer, composeEnhancers(
-        applyMiddleware(sagaMiddleware),
+        applyMiddleware(...middlewares),
     ));
 
     sagaMiddleware.run(rootSaga);
